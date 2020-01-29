2b7feba9e55bfbe96d807aebe78a94ba2eb62d52
Takuji Shimokawa <takuji.shimokawa@nulab-inc.com>
2020-01-29 16:04:40 +0900

    Modify to be compiled with haxe4 compiler

--- knockout/BindingContext.hx
+++ knockout/BindingContext.hx
@@ -2,14 +2,14 @@ package knockout;
 
 extern class BindingContext {
 
-    public var parent(get_parent, null):Null<Dynamic>;
-    public var parents(get_parents, null):Null<Array<Dynamic>>;
-    public var root(get_root, null):Dynamic;
-    public var data(get_data, null):Null<Dynamic>;
-    public var index(get_index, null):Null<Int>;
-    public var parentContext(get_parentContext, null):Null<BindingContext>;
-    public var context(get_context, null):BindingContext;
-    public var element(get_element, null):js.html.Node;
+    public var parent(get, null):Null<Dynamic>;
+    public var parents(get, null):Null<Array<Dynamic>>;
+    public var root(get, null):Dynamic;
+    public var data(get, null):Null<Dynamic>;
+    public var index(get, null):Null<Int>;
+    public var parentContext(get, null):Null<BindingContext>;
+    public var context(get, null):BindingContext;
+    public var element(get, null):js.html.Node;
 
     private inline function get_parent():Null<Dynamic> untyped {
         return this["$parent"];
